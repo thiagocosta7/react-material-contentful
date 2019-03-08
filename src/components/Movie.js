fb0a1a3afb3137e4d11ecd9678b8da9a280b1b04
Thiago Costa <wafx.thiago@grupofleury.com.br>
2019-03-08 19:03:20 -0300

    finishing app

---
+++ src/components/Movie.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+
+const Movie = (props) => {
+    return (
+        <div>
+            { props.movie ? (
+                <Card>
+                    <CardMedia
+                        style={{height: 0, paddingTop: '56.25%'}}
+                        image={props.movie.fields.image.fields.file.url}
+                        title={props.movie.fields.title}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="headline" component="h2">
+                            {props.movie.fields.title}
+                        </Typography>
+                        <Typography component="p">
+                            {props.movie.fields.description}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small" color="primary" href={props.movie.fields.url} target="_blank">
+                            Ver Trailer
+                        </Button>
+                    </CardActions>
+                </Card>
+            ) : null }
+        </div>
+    )
+}
+
+export default Movie
---
+++ src/components/MovieList.js
@@ -0,0 +1,87 @@
+import React, {Component} from 'react'
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Search from '@material-ui/icons/Search';
+import * as contentful from 'contentful'
+import Movie from '../components/Movie'
+
+//Contentful info
+const SPACE_ID = process.env.REACT_APP_CONTENTFUL_SPACE_ID
+const ACCESS_TOKEN = process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
+
+const client = contentful.createClient({
+    space: SPACE_ID,
+    accessToken: ACCESS_TOKEN
+})
+
+class MovieList extends Component {
+    state = {
+        movie: [],
+        searchString: ''
+    }
+
+    constructor() {
+        super()
+        this.getMovies()
+    }
+
+    getMovies = () => {
+        client.getEntries({
+            content_type: 'movie',
+            query: this.state.searchString
+        })
+        .then((response) => {
+            this.setState({ movies: response.items })
+        })
+        .catch((error) => {
+            console.log("Ocorreu um erro...")
+            console.log(error)
+        })
+    }
+
+    onSearchInputChange = (event) => {
+        if (event.target.value) {
+            this.setState({ searchString: event.target.value })
+        } else {
+            this.setState({ searchString: '' })
+        }
+        this.getMovies()
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.movies ? (
+                    <div>
+                        <TextField
+                            style={{margin: 24}}
+                            id="searchInput"
+                            placeholder="Insira o nome do filme..."
+                            margin="normal"
+                            onChange={this.onSearchInputChange}  
+                            variant="outlined"                      
+                            label="Pesquisar"
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <Search />
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />                        
+                        <Grid container spacing={24} style={{padding: 24}}>
+                            { this.state.movies.map(currentMovie => (
+                                <Grid item xs={12} sm={6} lg={4} xl={3}>
+                                    <Movie movie={currentMovie} />
+                                </Grid>
+                            )) }
+                        </Grid>
+                    </div>
+                ) : "Nenhum filme foi encontrado!"}
+            </div>
+        )
+    }
+}
+
+export default MovieList
